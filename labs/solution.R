c4f523b8c0afe770fec0e7de69960012ee22e0b6
mike.stackhouse <mike.stackhouse@atorusresearch.com>
2022-05-17 17:07:04 +0000

    Add labs example

---
+++ labs/solution.R
@@ -0,0 +1,92 @@
+# Labs Table
+# 
+# Date: 2022-05-16
+
+
+## Load packages ---
+library(Tplyr)
+library(pharmaRTF)
+library(dplyr)
+library(tidyr)
+
+## Read in necessary source data ----
+adsl <- haven::read_xpt(url("https://github.com/phuse-org/TestDataFactory/raw/main/Updated/TDF_ADaM/adsl.xpt"))
+adlb <- haven::read_xpt(url("https://github.com/phuse-org/TestDataFactory/raw/main/Updated/TDF_ADaM/adlbc.xpt"))
+
+## Labs Work Area ----
+
+# Create the Tplyr table object - this is like a specification for the table
+# Filter down to Calcium tests in the Safety group post-baseline
+t <- tplyr_table(adlb, TRTA, where = SAFFL == "Y" & PARAMCD == "CA" & !is.na(AVISITN) & AVISITN > 0) %>% 
+  set_pop_data(adsl) %>% 
+  set_pop_treat_var(TRT01A) %>% 
+  # Override the table filter for population data because target data filters
+  # have variables not in the population data
+  set_pop_where(SAFFL == "Y") %>% 
+  add_layer(
+    group_desc(vars(BASE, AVAL), by = AVISIT) %>% 
+      set_format_strings(
+        "N"        = f_str("xx", n),
+        "Mean"     = f_str('xx.a+1', mean), # automatically calculate decimal precision and add one decimal space
+        "SD"       = f_str('xxx.a+2', sd), # automatically calculate decimal precision and add two decimal spaces
+        "Median"   = f_str('xx.a+1', median),
+        "Q1, Q3"   = f_str('xx.a+1, x.a+1', q1, q3),
+        "Min, Max" = f_str("x.a, x.a", min, max), # automatically calculate decimal precision use that decimal precision
+        # Cap the decimal length 
+        cap = c(dec=2) # If limit auto-collected precision to 2 decimal spaces
+      )
+    )
+
+# Now build the table - this is where the number crunching happens
+labs1 <- t %>% 
+  build()
+
+# View the data 
+labs1
+
+# Now let's make it pretty
+labs2 <- labs1 %>% 
+  apply_row_masks(row_breaks = TRUE, ord_layer_1) %>% # Blank out repeating row labels and insert row breaks
+  select(row_label1, row_label2, var1_Placebo, var2_Placebo, 
+         `var1_Xanomeline Low Dose`, `var2_Xanomeline Low Dose`,
+         `var1_Xanomeline High Dose`, `var2_Xanomeline High Dose`) %>% # Take only the columns we need
+  add_column_headers(
+    paste0("Timepoint| | Placebo\\line(N=**Placebo**) {At Timepoint | Change from Baseline}",
+           "| Xanomeline Low Dose\\line(N=**Xanomeline Low Dose**) {At Timepoint | Change from Baseline} |",
+           "Xanomeline High Dose\\line(N=**Xanomeline High Dose**) {At Timepoint | Change from Baseline}"),
+    header_n = header_n(t)
+    ) # Huxtable-ready column headers and header Ns
+  
+# This is a table styling library named Huxtable that has good RTF capabilities
+ht <- huxtable::as_hux(labs2, add_colnames=FALSE) %>% # `add_colnames` is FALSE because we already added our own
+  huxtable::merge_cells(1, 3:4) %>% # Merges cells for placebo
+  huxtable::merge_cells(1, 5:6) %>% # Merges cells for Low Xanomeline Dose
+  huxtable::merge_cells(1, 7:8) %>% # Merges cells for High Xanomeline Dose
+  huxtable::set_bold(1:2, 1:ncol(labs2), TRUE) %>% # Bold the header rows
+  huxtable::set_align(1:2, 3:ncol(labs2), 'center') %>% # Center align the results headers
+  huxtable::set_align(3:nrow(labs2), 3:ncol(labs2), 'center') %>% # Center align the results
+  huxtable::set_valign(1:2, 1:ncol(labs2), 'bottom') %>% # Bottom align the header rows
+  huxtable::set_top_border(1, 1:ncol(labs2), 1) %>% # Put a border above the header row
+  huxtable::set_bottom_border(2, 1:ncol(labs2), 1) %>% # Put a border under the header row
+  huxtable::set_width(1.5) %>% # Set the table width
+  huxtable::set_escape_contents(FALSE) %>% # Don't escape RTF syntax
+  huxtable::set_col_width(c(.11, .11, .13, .13, .13, .13, .13, .13)) # Set the column widths
+
+# Now we'll use pharmaRTF to create the RTF output 
+# Two header rows because we have two rows of column headers
+doc <- rtf_doc(ht, header_rows=2) %>%
+  # This gets our rows nice and close together - find more info in the docs
+  set_ignore_cell_padding(TRUE) %>% 
+  set_font_size(9) %>% 
+  # Add in a title
+  add_titles(
+    hf_line("Summary of Change from Baseline Results: Calcium (mmol/L)", bold=TRUE, align = 'left', font_size = 11),
+    hf_line("Safety Population", bold=TRUE, align = 'left', font_size = 11)
+  ) %>%
+  add_footnotes(
+    hf_line(paste0("Abbreviations: N = number of subjects in the population; Q1=25th Percentile;",
+                   " Q3= 75th Percentile; SD=Standard Deviation."), bold = TRUE, align='left', font_size = 9)
+  )
+
+# Now we can write out the RTF file! 
+write_rtf(doc, here::here("labs", "labs.rtf"))
